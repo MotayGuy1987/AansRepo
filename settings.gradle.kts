535b4fe9ac51017e46c28716ab8722d882611a27
MotayGuy1987 <geometrydashdotcom@gmail.com>
2025-12-01 18:23:09 -0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,7 @@
-rootProject.name = "CloudstreamPlugins"
+rootProject.name = "AansRepo"
 
-// This file sets what projects are included.
-// All new projects should get automatically included unless specified in the "disabled" variable.
-
-val disabled = listOf<String>()
-
-File(rootDir, ".").eachDir { dir ->
-    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
-        include(dir.name)
-    }
-}
-
-fun File.eachDir(block: (File) -> Unit) {
-    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
-}
-
-// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-// include("PluginName")
+include(":MovieHaxBiz")
+include(":HDHub4uRehab")
+include(":HindiLinks4uGarden")
+include(":Filmyzilla27")
+include(":CinebyGd")
